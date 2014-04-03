e9d6e6acc07800e5525553de3076376832d6c541
Karan Veer Kajla <veer@lnxsrv02.seas.ucla.edu>
2014-04-03 00:28:06 -0700

    added stack command stack implementation

--- read-command.c
+++ read-command.c
@@ -24,11 +24,11 @@ enum token_type
 	ENDOFFILE,
 };
 
-struct token
+typedef struct token
 {
 	enum token_type type;
 	char* word;
-};
+}token;
 
 /*struct command_stream
 {
@@ -38,17 +38,47 @@ struct token
 	int size; //the total size of the array, changes with reallocation
 }*/
 
-typedef struct command_stream
-{
-	struct command_node *head;
-	struct command_node *tail;
-}command_stream;
-
 typedef struct command_node
 {
 	struct command_t *command;
 	struct command_node *next;
 }command_node;
+
+typedef struct command_stream
+{
+	command_node *head;
+	command_node *tail;
+}command_stream;
+
+typedef struct command_stack_node
+{
+	command_t *cmd;
+	command_stack_node* prev;
+}command_stack_node;
+
+typedef struct command_stack
+{
+	command_node *top;
+	command_node *bottom;
+}command_stack;
+
+command_stack_node* push (command_stack_node *stack, command_t *cmd)
+{
+	command_stack_node* newtop = (command_stack_node*) checked_malloc(sizeof(command_stack_node));
+	newtop->cmd = cmd;
+	newtop->prev = stack->top;
+	stack->top = newtop;
+	return stack->top;
+}
+
+command_t* pop (command_stack_node *stack)
+{
+	command_stack_node *temp = stack->top;
+	top = top->prev;
+	cmd* val = temp->cmd;
+	free(temp);
+	return val;
+}
 
 command_node insert_node (command_node *tail, command_t cmd)
 {
@@ -57,6 +87,24 @@ command_node insert_node (command_node *tail, command_t cmd)
 	node->next = NULL;
 	tail->next = node;
 	tail = node;
+}
+
+int precedence(enum command_type op)
+{
+	switch(op)
+	{
+		case SEQUENCE_COMMAND:
+			return 0;
+		case AND_COMMAND:
+		case OR_COMMAND:
+			return 1;
+		case PIPE_COMMAND:
+			return 2;
+		case SUBSHELL_COMMAND:
+			return 3;
+		default:
+			return -1;
+	}
 }
 
 command_stream_t
